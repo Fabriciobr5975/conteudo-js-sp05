252c5274ffb4bac88f8c8340bc6e5364ca088cb0
Fabriciobr5975 <araujofabricio71@gmail.com>
2026-07-13 22:06:56 -0300

    feat: Adicionando o conteúdo da aula 03 de JavaScript

---
+++ aula03/src/script.js
@@ -0,0 +1,99 @@
+// Declarando variáveis com let
+let carro = "Gol";
+let moto = "CB500";
+let cor = "Azul";
+
+console.log(carro);
+console.log(moto);
+console.log(cor);
+
+console.log("-----------------");
+
+// Declarando variáveis com const
+
+const preco1 = 10;
+const preco2 = 20;
+let valorTotal = preco1 + preco2;
+
+console.log(preco1);
+console.log(preco2);
+
+console.log("O valor total é: " + valorTotal);
+
+// Declarando variáveis de forma automática
+x = 1;
+y = 6;
+z = x + y;
+
+console.log(z);
+
+console.log("")
+
+// Declarando variáveis com var;
+
+var fruta1 = "Abacate";
+var fruta2 = "Maça";
+var fruta3 = "Morango";
+
+console.log(fruta1);
+console.log(fruta2);
+console.log(fruta3);
+
+console.log("");
+
+// Tipos de dados em variáveis (números e string)
+
+let numero1 = 4;
+let numero2 = 7;
+let numero3 = "4";
+let numero4 = "7";
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+
+let soma1 = numero1 + numero2;
+
+console.log(soma1);
+
+let soma2 = numero3 + numero4;
+
+console.log(soma2);
+
+console.log("");
+
+// Declarando variáveis em uma única instrução
+
+let nomePessoa = "Fabrício", idade = "21", cidade = "Osasco";
+
+console.log(nomePessoa);
+console.log(idade);
+console.log(cidade);
+
+// let num1 = 5;
+// console.log(num1);
+// num1 = num1 + 5;
+// console.log(num1);
+
+console.log("");
+
+// Operações aritmética
+
+let soma = 5 + 3 + 2;
+console.log(soma);
+
+let nomeConcatenado = "Fabrício" + " " + "Araújo"
+console.log(nomeConcatenado);
+
+console.log("");
+
+let valorPrincipal = 20;
+console.log(valorPrincipal)
+
+{
+    let valorPrincipal = 5;
+    console.log(valorPrincipal);
+}
+
+console.log(valorPrincipal);
