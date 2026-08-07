8d4cb4f901457a99cd0b8bdd53b043e8ebcc8121
Fabriciobr5975 <araujofabricio71@gmail.com>
2026-08-06 22:22:04 -0300

    feat: Pequena melhorada na animação. Agora ela volta ao estado original

--- aula17/src/script03.js
+++ aula17/src/script03.js
@@ -7,17 +7,26 @@ function movimentoAnimacao() {
     const elemento = document.querySelector(".animacao");
     let id = null;
     let posicao = 0;
+    let inverter = false;
     clearInterval(id);
     id = setInterval(quadro, 10);
 
     function quadro() {
-        if (posicao === 300) {
-            clearInterval(id);
-            elementoBotao.disabled = false;
+        if (posicao === 300 || inverter) {
+            posicao--;
+            elemento.style.top = posicao + "px";
+            elemento.style.left = posicao + "px";
+            if (posicao === 0) {
+                elementoBotao.disabled = false;
+                clearInterval(id);
+                inverter = false;
+            }
         } else {
             posicao++;
             elemento.style.top = posicao + "px";
             elemento.style.left = posicao + "px";
+
+            if (posicao >= 299) inverter = true;
         }
     }
 }
